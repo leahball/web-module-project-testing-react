d89705283156105aa31746960452b648ec78a0d8
iamleahb <leah.ball@gmail.com>
2022-01-24 20:55:59 -0600

    display suite created, mvp met

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Display from './../Display';
+import Display from "./../Display";
 
+import mockFetchShow from "./../../api/fetchShow";
+jest.mock("./../../api/fetchShow");
 
-test('renders without errors with no props', ()=>{
+const testShow = {
+  name: "test show",
+  summary: "test summary",
+  seasons: [
+    {
+      id: 0,
+      name: "season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "season 2",
+      episodes: [],
+    },
+  ],
+};
 
+test("renders without errors with no props", () => {
+  render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test("renders Show component when the button is clicked ", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const show = await screen.findByTestId("show-container");
+  expect(show).toBeInTheDocument();
+});
+
+test("renders show season options matching your data when the button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const seasonOptions = screen.queryAllByTestId("season-option");
+    expect(seasonOptions).toHaveLength(2);
+  });
+});
